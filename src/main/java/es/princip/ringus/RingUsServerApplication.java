17c55a35887fbd672826361e15df4c132a770993
wlgns12370 <knu12370@knu.ac.kr>
2024-12-29 18:15:57 +0900

    init: ring-us-server 개발 환경 구축

---
+++ src/main/java/es/princip/ringus/RingUsServerApplication.java
@@ -0,0 +1,13 @@
+package es.princip.ringus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingUsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingUsServerApplication.class, args);
+    }
+
+}
