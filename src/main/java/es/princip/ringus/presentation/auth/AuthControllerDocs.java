7b2c454d417f9ad26c786a053600b09a8ae0ec8f
bohyeong <95080710+jbh010204@users.noreply.github.com>
2025-02-21 03:32:33 +0900

    RINGUS-25 feat: 쿠키 적용 및 회원가입 세부 정보 추가 (#4)
    * RINGUS-25 feat: 멘토, 멘티 프로필 등록
    
    * RINGUS-25 feat: 서비스 약관 동의 여부 API 구현
    
    * RINGUS-25 feat: swagger 적용 및 Auth 관련 API 적용
    
    * RINGUS-25 feat: 이메일 인증 시 쿠키 지급
    
    * RINGUS-25 feat: 회원가입 및 로그인 시 쿠키 지급 기능 구현
    
    * RINGUS-25 feat: 학교 이메일 여부 판별 기능 추가
    
    * RINGUS-25 feat: 멤버 로그아웃 기능 추가
    
    * RINGUS-25 refactor: 정적 변수 값을 환경변수로 분리
    
    * RINGUS-25 feat: 로그인 이후 API 요청시 세션 검증 기능 구현
    
    * RINGUS-25 feat: 세션 검증 API 구현

--- src/main/java/es/princip/ringus/presentation/auth/AuthControllerDocs.java
+++ src/main/java/es/princip/ringus/presentation/auth/AuthControllerDocs.java
@@ -1,7 +1,5 @@
 package es.princip.ringus.presentation.auth;
 
-import es.princip.ringus.domain.exception.SignUpErrorCode;
-import es.princip.ringus.global.exception.ErrorCode;
 import es.princip.ringus.global.util.ApiResponseWrapper;
 import es.princip.ringus.presentation.auth.dto.request.EmailVerifyRequest;
 import es.princip.ringus.presentation.auth.dto.request.GenerateCodeRequest;
@@ -9,15 +7,14 @@ import es.princip.ringus.presentation.auth.dto.request.LoginRequest;
 import es.princip.ringus.presentation.auth.dto.request.SignUpRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,12 +31,12 @@ public interface AuthControllerDocs {
             @ApiResponse(responseCode = "400", description = "잘못된 요청")
     })
     @PostMapping("/signup")
-    ResponseEntity<?> signUp(@Valid @RequestBody @Parameter SignUpRequest request);
+    ResponseEntity<?> signUp(@Valid @RequestBody @Parameter SignUpRequest request, HttpSession session, HttpServletResponse httpResponse);
 
     @Operation(summary = "로그인", description = "로그인을 처리합니다.")
     @ApiResponse(responseCode = "200", description = "로그인 성공")
     @PostMapping("/login")
-    ResponseEntity<ApiResponseWrapper<Void>> login(@Valid @RequestBody @Parameter LoginRequest request, HttpSession session);
+    ResponseEntity<ApiResponseWrapper<Void>> login(@Valid @RequestBody @Parameter LoginRequest request, HttpSession session, HttpServletRequest httpRequest, HttpServletResponse httpResponse);
 
     @Operation(summary = "이메일 인증 코드 요청", description = "이메일 인증번호를 발급합니다.")
     @ApiResponse(responseCode = "200", description = "인증번호 발급 성공")
@@ -49,5 +46,5 @@ public interface AuthControllerDocs {
     @Operation(summary = "이메일 인증 코드 검증", description = "인증번호를 검증합니다.")
     @ApiResponse(responseCode = "200", description = "인증 성공")
     @PostMapping("/email/code/verify")
-    ResponseEntity<ApiResponseWrapper<Void>> verifyCode(@Valid @RequestBody @Parameter EmailVerifyRequest request);
+    ResponseEntity<ApiResponseWrapper<Void>> verifyCode(@Valid @RequestBody @Parameter EmailVerifyRequest request, HttpSession session, HttpServletResponse httpResponse);
 }
