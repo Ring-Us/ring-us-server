e2494db538a51fec1d3c366bba8222d669861590
Jaeyeong Park <119722262+wodydl0@users.noreply.github.com>
2025-02-28 16:42:46 +0900

    RINGUS-44 refactor: 파일 로드 api 리팩토링 (#28)
    * RINGUS-44 feat: 증명서 및 이미지 로직 관심사 분리
    
    * RINGUS-44 feat: 프로필 이미지 조회 api 및 에러코드 작성
    
    * RINGUS-44 fix: 조회 기능 삭제

--- src/main/java/es/princip/ringus/infra/storage/api/CertificateController.java
+++ src/main/java/es/princip/ringus/infra/storage/api/CertificateController.java
@@ -3,47 +3,42 @@ package es.princip.ringus.infra.storage.api;
 import es.princip.ringus.domain.exception.MemberErrorCode;
 import es.princip.ringus.global.exception.CustomRuntimeException;
 import es.princip.ringus.global.util.ApiResponseWrapper;
-import es.princip.ringus.infra.storage.application.StorageService;
+import es.princip.ringus.infra.storage.application.StorageCertificateService;
 import es.princip.ringus.infra.storage.dto.CertificateUploadRequest;
-import es.princip.ringus.infra.storage.dto.ProfileUploadRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/storage")
+@RequestMapping("/storage/certificate")
 @RequiredArgsConstructor
-public class StorageController {
+public class CertificateController {
 
-    private final StorageService storageService;
-
+    private final StorageCertificateService storageCertificateService;
     /**
      * 멘티 증명서 업로드
      */
-    @PostMapping("/certificate/mentee")
+    @PostMapping("/mentee")
     public ResponseEntity<ApiResponseWrapper<Void>> uploadMenteeCertificate(
             @ModelAttribute CertificateUploadRequest certificateUploadRequest,
             HttpSession session
-            ) {
+    ) {
 
         Long memberId = (Long)session.getAttribute("memberId");
         if(memberId == null){
             throw new CustomRuntimeException(MemberErrorCode.SESSION_EXPIRED);
         }
 
-        String filePath = storageService.uploadMenteeCertificate(certificateUploadRequest,memberId);
+        String filePath = storageCertificateService.uploadMenteeCertificate(certificateUploadRequest,memberId);
         return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, filePath));
     }
 
     /**
      * 멘토 증명서 업로드
      */
-    @PostMapping("/certificate/mentor")
+    @PostMapping("/mentor")
     public ResponseEntity<ApiResponseWrapper<Void>> uploadMentorCertificate(
             @ModelAttribute CertificateUploadRequest certificateUploadRequest,
             HttpSession session
@@ -53,25 +48,8 @@ public class StorageController {
             throw new CustomRuntimeException(MemberErrorCode.SESSION_EXPIRED);
         }
 
-        String filePath = storageService.uploadMentorCertificate(certificateUploadRequest, memberId);
+        String filePath = storageCertificateService.uploadMentorCertificate(certificateUploadRequest, memberId);
         return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, filePath));
     }
 
-    /**
-     * 프로필 이미지 업로드
-     */
-    @PostMapping("/profile/image")
-    public ResponseEntity<ApiResponseWrapper<Void>> uploadProfileImage(
-            @ModelAttribute ProfileUploadRequest request,
-            HttpSession session
-            ) {
-
-        Long memberId = (Long)session.getAttribute("memberId");
-        if(memberId == null){
-            throw new CustomRuntimeException(MemberErrorCode.SESSION_EXPIRED);
-        }
-
-        String filePath = storageService.uploadProfileImage(request);
-        return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, filePath));
-    }
 }
---
+++ src/main/java/es/princip/ringus/infra/storage/api/ProfileImageController.java
@@ -0,0 +1,39 @@
+package es.princip.ringus.infra.storage.api;
+
+import es.princip.ringus.domain.exception.MemberErrorCode;
+import es.princip.ringus.global.exception.CustomRuntimeException;
+import es.princip.ringus.global.util.ApiResponseWrapper;
+import es.princip.ringus.infra.storage.application.StorageProfileImageService;
+import es.princip.ringus.infra.storage.dto.ProfileUploadRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/storage/profile")
+@RequiredArgsConstructor
+public class ProfileImageController {
+
+    private final StorageProfileImageService storageProfileService;
+
+    /**
+     * 프로필 이미지 업로드
+     */
+    @PostMapping("/image")
+    public ResponseEntity<ApiResponseWrapper<Void>> uploadProfileImage(
+            @ModelAttribute ProfileUploadRequest request,
+            HttpSession session
+    ) {
+
+        Long memberId = (Long)session.getAttribute("memberId");
+        if(memberId == null){
+            throw new CustomRuntimeException(MemberErrorCode.SESSION_EXPIRED);
+        }
+
+        String filePath = storageProfileService.uploadProfileImage(request);
+        return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, filePath));
+    }
+
+}
