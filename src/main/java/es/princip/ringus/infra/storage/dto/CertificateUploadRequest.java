d64a66f24b9ac311f3cb291653807cc35c589ae1
wodydl0 <poty9798@naver.com>
2025-03-01 23:50:52 +0900

    feat: storage swagger docs 작성

--- src/main/java/es/princip/ringus/infra/storage/dto/CertificateUploadRequest.java
+++ src/main/java/es/princip/ringus/infra/storage/dto/CertificateUploadRequest.java
@@ -2,10 +2,14 @@ package es.princip.ringus.infra.storage.dto;
 
 import es.princip.ringus.infra.storage.domain.CertificateType;
 import es.princip.ringus.infra.storage.domain.FileMember;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.web.multipart.MultipartFile;
 
+@Schema(description = "증명서 업로드 요청 데이터")
 public record CertificateUploadRequest(
+        @Schema(description = "파일", required = true, example = "file.pdf")
         MultipartFile file,
+        @Schema(description = "증명서 유형 ENUM", required = true, example = "ENROLLMENT")
         CertificateType certificateType
 ) {
     // form-data 형식에서는 record 클래스는 자동 바인딩이 안돼서 생성자를 명시적으로 만들어줘야 함
