0eec874e1ea8265e07d315ff029b21f6ca9c278a
bohyeong <jbh010204@knu.ac.kr>
2025-03-03 21:15:08 +0900

    RINGUS-48 chore: 멘토 엔티티 변경에 따른 flyway 스키마 변경

---
+++ src/main/resources/db/migration/V2__insert_term.sql
@@ -0,0 +1,5 @@
+-- `service_term` 테이블에 3개 데이터 삽입
+INSERT INTO `service_term` (`service_term_tag`, `required`) VALUES
+    ('TERMS_OF_SERVICE', 1),
+    ('PRIVACY_POLICY', 1),
+    ('MARKETING_CONSENT', 0);
