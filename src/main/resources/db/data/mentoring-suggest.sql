e8288c9fd5d7cd60803514ac9b235f1d0c19b663
bohyeong <jbh010204@knu.ac.kr>
2025-03-22 20:55:53 +0900

    RINGUS-64 test: 멘토링 테스트 sql 작성

---
+++ src/main/resources/db/data/mentoring-suggest.sql
@@ -0,0 +1,9 @@
+INSERT INTO mentoring (created_at, updated_at, mentoring_status, mentoring_topic, apply_times, mentoring_message, mentee_id, mentor_id) VALUES
+    ('2025-03-10T10:00:00', '2025-03-10T10:00:00', 'WAITING', 'STUDY',
+     '[{"startTime": "2025-03-30T10:00:00", "endTime": "2025-03-30T10:30:00"},
+       {"startTime": "2025-03-30T15:00:00", "endTime": "2025-03-30T15:30:00"}]',
+     '멘토링 1', 1, 3),
+    ('2025-03-10T10:00:00', '2025-03-10T10:00:00', 'WAITING', 'STUDY',
+     '[{"startTime": "2025-03-30T10:00:00", "endTime": "2025-03-30T10:30:00"},
+       {"startTime": "2025-03-30T15:00:00", "endTime": "2025-03-30T15:30:00"}]',
+     '멘토링 1', 1, 4);
