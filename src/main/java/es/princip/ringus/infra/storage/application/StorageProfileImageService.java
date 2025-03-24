9c52d61843c94586059a4c88e01dac56fe3c7329
wodydl0 <poty9798@naver.com>
2025-03-24 10:08:53 +0900

    RINGUS-65 fix :이미지 파일 로직 fileMember에 추가하도록 변경

--- src/main/java/es/princip/ringus/infra/storage/application/StorageProfileImageService.java
+++ src/main/java/es/princip/ringus/infra/storage/application/StorageProfileImageService.java
@@ -1,6 +1,7 @@
 package es.princip.ringus.infra.storage.application;
 
 import es.princip.ringus.global.util.StoragePathUtil;
+import es.princip.ringus.infra.storage.domain.FileMemberRepository;
 import es.princip.ringus.infra.storage.dto.ProfileUploadRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,14 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class StorageProfileImageService {
 
     private final S3Service s3Service;
+    private final FileMemberRepository fileMemberRepository;
 
     /**
      * 프로필 이미지 업로드
      */
     @Transactional
-    public String uploadProfileImage(ProfileUploadRequest request) {
+    public String uploadProfileImage(ProfileUploadRequest request, Long memberId) {
         String folderPath = StoragePathUtil.buildProfileFolderPath(request.memberType());
-        return s3Service.uploadFile(request.file(), folderPath);
+
+        String filePath = s3Service.uploadFile(request.file(), folderPath, true);
+
+        fileMemberRepository.save(request.toFileMemberEntity(filePath, memberId));
+
+        return filePath;
     }
 
 }
