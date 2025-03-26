ad46ed672ccc0bb94e3f4f3c0d409b2fca6e3088
wodydl0 <poty9798@naver.com>
2025-03-26 22:22:04 +0900

    RINGUS-65 feat: api Docs 설정

--- src/main/java/es/princip/ringus/infra/storage/api/FileControllerDocs.java
+++ src/main/java/es/princip/ringus/infra/storage/api/FileControllerDocs.java
@@ -4,8 +4,6 @@ import es.princip.ringus.global.util.ApiResponseWrapper;
 import es.princip.ringus.infra.storage.dto.FilePreviewResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
