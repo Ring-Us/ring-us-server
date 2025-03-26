5c3ef0299f16b13c5b32d682c5e380a685140b0e
wlgns12370 <knu12370@knu.ac.kr>
2025-03-26 21:03:29 +0900

    RINGUS-70 fix: 쿼리Dsl 생성자 주입 추가

--- src/main/java/es/princip/ringus/domain/bookmark/BookmarkQueryDslRepositoryImpl.java
+++ src/main/java/es/princip/ringus/domain/bookmark/BookmarkQueryDslRepositoryImpl.java
@@ -1,9 +1,13 @@
 package es.princip.ringus.domain.bookmark;
 
 import es.princip.ringus.domain.support.QueryDslSupport;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 import static es.princip.ringus.domain.mentoring.QMentoring.mentoring;
 
+@Repository
+@RequiredArgsConstructor
 public class BookmarkQueryDslRepositoryImpl extends QueryDslSupport implements BookmarkQueryDslRepository {
     @Override
     public Boolean isBookmarked(Long memberId, Long mentorId) {
--- src/main/java/es/princip/ringus/domain/mentoring/MentoringQueryDslRepositoryImpl.java
+++ src/main/java/es/princip/ringus/domain/mentoring/MentoringQueryDslRepositoryImpl.java
@@ -1,9 +1,13 @@
 package es.princip.ringus.domain.mentoring;
 
 import es.princip.ringus.domain.support.QueryDslSupport;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 import static es.princip.ringus.domain.mentoring.QMentoring.mentoring;
 
+@Repository
+@RequiredArgsConstructor
 public class MentoringQueryDslRepositoryImpl extends QueryDslSupport implements MentoringQueryDslRepository {
 
     @Override
