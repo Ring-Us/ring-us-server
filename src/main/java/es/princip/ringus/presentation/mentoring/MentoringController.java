3643b8403bb3be639460b2e3e8cf05c58db1b550
bohyeong <jbh010204@knu.ac.kr>
2025-03-28 14:38:31 +0900

    RINGUS-71 feat: 멘티가 신청한 멘토링 시간대 변경 기능 구현

--- src/main/java/es/princip/ringus/presentation/mentoring/MentoringController.java
+++ src/main/java/es/princip/ringus/presentation/mentoring/MentoringController.java
@@ -5,16 +5,20 @@ import es.princip.ringus.global.annotation.SessionCheck;
 import es.princip.ringus.global.annotation.SessionMemberId;
 import es.princip.ringus.global.util.ApiResponseWrapper;
 import es.princip.ringus.presentation.mentoring.dto.CreateMentoringRequest;
+import es.princip.ringus.presentation.mentoring.dto.EditMentoringTimeRequest;
 import es.princip.ringus.presentation.mentoring.dto.MentoringResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/mentoring")
 @RequiredArgsConstructor
@@ -30,4 +34,13 @@ public class MentoringController implements MentoringControllerDocs {
         MentoringResponse response = mentoringService.createMentoring(request, memberId);
         return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, "성공", response));
     }
+
+    @SessionCheck
+    @PatchMapping("/edit/date")
+    public ResponseEntity<ApiResponseWrapper<MentoringResponse>> changeDate(
+            @Valid @RequestBody EditMentoringTimeRequest request) {
+        mentoringService.changeMentoringDate(request);
+        return ResponseEntity.ok(ApiResponseWrapper.success(HttpStatus.OK, "성공", null));
+    }
+
 }
