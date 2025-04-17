9bf489e09bae3af0425796c991f439c5c0436aea
Jaeyeong Park <119722262+wodydl0@users.noreply.github.com>
2025-04-17 19:40:01 +0900

    RINGUS-77 feat: TimeTraceAop 적용 (#61)
    * RINGUS-65 feat: S3 증명서 접근 권한 분리 및 presigned url 적용
    
    * RINGUS-65 feat: api Docs 설정
    
    * RINGUS-77 feat: TimeTraceAop 설정

---
+++ src/main/java/es/princip/ringus/global/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package es.princip.ringus.global.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* es.princip.ringus..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            log.info("END:{} {}ms",joinPoint.toString(), timeMs);
+        }
+    }
+}
