f242df7b1d593ac0dfc35d82c6ccce0b4fb3471f
wodydl0 <poty9798@naver.com>
2025-05-19 04:41:55 +0900

    feat: S3 파일 업로드 수정

--- src/main/java/es/princip/ringus/presentation/common/dto/PortfolioRequest.java
+++ src/main/java/es/princip/ringus/presentation/common/dto/PortfolioRequest.java
@@ -4,9 +4,10 @@ import es.princip.ringus.domain.mentor.vo.Portfolio;
 
 public record PortfolioRequest(
         String url,
-        String description
+        String description,
+        Integer fileSize
 ) {
     public Portfolio toEntity() {
-        return new Portfolio(url, description);
+        return new Portfolio(url, description, fileSize);
     }
 }
